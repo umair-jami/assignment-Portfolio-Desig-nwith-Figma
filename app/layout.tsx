861c5094cf51c413bd4214676a15d54c4c122154
umair-jami <lalaumair92@gmail.com>
2024-11-22 09:36:46 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const icon="/portfolioIcon.png"
+const icon="/cv.png"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
